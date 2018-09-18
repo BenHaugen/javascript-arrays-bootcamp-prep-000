1774c720a378d01867bf9dc78925821502fe6768
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-18 04:24:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function removeElementFromEndOfArray(array) {
+  array.slice(array.length - 1)
+}
